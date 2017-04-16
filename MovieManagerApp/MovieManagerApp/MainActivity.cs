a7e32b3d45538e89dd999bba740f3fa52eda7f3f
gregfield <gregfield1996@gmail.com>
2017-04-16 17:51:12 +1200

    Main,NewRand,Seen Activities created

---
+++ MovieManagerApp/MovieManagerApp/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace MovieManagerApp
+{
+    [Activity(Label = "MovieManagerApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //Initilise Buttons
+            Button newRandBtn = FindViewById<Button>(Resource.Id.NewRandBtn);
+            Button viewAlreadySeenBtn = FindViewById<Button>(Resource.Id.AlreadyWatchedBtn);
+
+            //Adds the Click handlers to the buttons
+            newRandBtn.Click += NewRandBtn_Click;
+            viewAlreadySeenBtn.Click += ViewAlreadySeenBtn_Click;
+        }
+
+        /// <summary>
+        /// Button click handler for getting a new random movie.
+        /// Creates new activity and sends user to it.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewRandBtn_Click(object sender, System.EventArgs e)
+        {
+            //Make new Intent
+            Intent randMovieIntent = new Intent(this, typeof(RandMovieActivity));
+            //Start Activity
+            StartActivity(randMovieIntent);
+        }
+
+        /// <summary>
+        /// Button click handler for View the already seen movies.
+        /// Creates an information fragment with movie information
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ViewAlreadySeenBtn_Click(object sender, System.EventArgs e)
+        {
+            //Make new Intent
+            Intent seenMoviesIntent = new Intent(this, typeof(SeenMoviesActivity));
+            //Start Activity
+            StartActivity(seenMoviesIntent);
+        }
+    }
+}
+
---
+++ MovieManagerApp/MovieManagerApp/SeenMoviesActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MovieManagerApp
+{
+    [Activity(Label = "Seen Movies Activity")]
+    public class SeenMoviesActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.SeenMoviesLayout);
+
+            //Get list view so can be used
+            ListView seenMoviesListView = FindViewById<ListView>(Resource.Id.seenMoviesListView);
+            //Fills the list view with data
+            FillListView(seenMoviesListView);
+            //adds an item click listener
+            seenMoviesListView.ItemClick += SeenMoviesListView_ItemClick;
+            
+        }
+
+        /// <summary>
+        /// Fills the list view with the relevant data
+        /// </summary>
+        private void FillListView(ListView seenMoviesListView)
+        {
+            //Seen Movieslist
+            string[] movieslist = { "fast and furious 1", "fast and furious 2",
+                "fast and furious 3", "fast and furious 4", "fast and furious 5" };
+
+            //Makes and adapter so data can be put into listView
+            ArrayAdapter<string> seenMoviesAdapter = new ArrayAdapter<string>(this, 
+                Resource.Layout.ListViewLayout, Resource.Id.movieTitle, movieslist);
+
+            //Gets the listview and sets the adapter
+            seenMoviesListView.Adapter = seenMoviesAdapter;
+        }
+
+        /// <summary>
+        /// Decides what happens when an item is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SeenMoviesListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Toast.MakeText(this, e.Position.ToString(), ToastLength.Short).Show();
+        }
+    }
+}
